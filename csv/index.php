831e7e1127b36cdcd014aa4ba07df5ed75dffbb3
jsncd <41863661+jsncd@users.noreply.github.com>
2018-07-29 16:06:07 +0300

    Update index.php

--- csv/index.php
+++ csv/index.php
@@ -52,10 +52,7 @@ $result = mysqli_query($connect, $query);
 <!DOCTYPE html>
 <html>
  <head>
-  <title>Update Mysql Database through Upload CSV File using PHP</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <title>Update Mysql Database </title>
  </head>
  <body>
   <br />
@@ -63,14 +60,14 @@ $result = mysqli_query($connect, $query);
    <h2 align="center">Update Mysql Database through Upload CSV File using PHP</a></h2>
    <br />
    <form method="post" enctype='multipart/form-data'>
-    <p><label>Please Select File(Only CSV Formate)</label>
-    <input type="file" name="product_file" /></p>
+    <p><label>Please Select File(CSV)</label>
+    <input type="file" name="file" /></p>
     <br />
     <input type="submit" name="upload" class="btn btn-info" value="Upload" />
    </form>
    <br />
    <?php echo $message; ?>
-   <h3 align="center">Deals of the Day</h3>
+   <h3 align="center">Table</h3>
    <br />
    <div class="table-responsive">
     <table class="table table-bordered table-striped">
